896aa45e847ac229caf0d29d8610e2f65a017660
Ross Holtrop <holtrop.ross@gmail.com>
2021-07-25 15:42:47 -0700

    working application. deployed for readme creationg.

--- index.js
+++ index.js
@@ -1,13 +1,80 @@
 // TODO: Include packages needed for this application
+    const inquirer = require("inquirer");
+    const fs = require("fs");
+    const util = require('util');
+    const generateMD = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+        {
+            type: 'input',
+            name: 'welcome',
+            message: 'Welcome to my automated README.md Generator. \n I\'m going to ask you some questions that will help me complete your customized README.md. \n Let\'s get started! Press ENTER to continue.',
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project, exactly as it appears on GitHub?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Input your project description.',
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'Input your project installation instructions.',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Input your project usage information. How will this be used? ',
+        },
+        {
+            type: 'input',
+            name: 'guideline',
+            message: 'Input your project contribution guidelines.',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Input your project test instructions. How can another user test the gerator markdown?',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Which license applies to your project?',
+            choices: ['Apache-2.0', 'BSD-2-Clause', 'BSD-3-Clause', 'CDDL-1.0', 'EPL-2.0', 'GPL-2.0', 'LGPL-3.0', 'MIT', 'MPL-2.0', 'N/A'],
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your GitHub username?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+        },
+    ]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
 
+    //prompts
+    inquirer.prompt(questions)
+    //store responses
+    .then((answers) => {
+        //then create README
+        writeToFile('readme.md', generateMarkdown(answers))
+        console.log("README is generated")
+    })
+}
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,8 +12,35 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # Welcome to my automated README.md Generator
 
+  ## What is the title of your project, exactly as it appears on GitHub?
+  * ${data.title} #
+
+  ## Describe your project? 
+  * ${data.description} 
+
+  ## Input your project installation instructions? 
+  * ${data.install} 
+
+  ## Input your project usage information. How will this be used? 
+  * ${data.usage} 
+
+  ## Input your project contribution guidelines.
+  * ${data.guideline}
+  
+  ## Input your project test instructions. How can another user test the gerator markdown? 
+  * ${data.test}
+
+  ## Which license applies to your project?
+  * ${data.license}
+
+  // ## What is your GitHub username? 
+  * ${data.github} 
+
+  ## What is your email address? 
+  * ${data.email} 
 `;
 }
 
